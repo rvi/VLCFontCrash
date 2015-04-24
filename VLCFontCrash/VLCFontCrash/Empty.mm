e160d7a0e3ca25176cb6772c6aa56f4f1b84b1bb
Rémy Virin <remy@remyvirin.com>
2015-04-24 11:02:20 +0200

    add Objective-C++ file to force the link with C++ standard librairies

---
+++ VLCFontCrash/VLCFontCrash/Empty.mm
@@ -0,0 +1,9 @@
+//
+//  Empy.m
+//  VLCFontCrash
+//
+//  Created by Rémy Virin on 24/04/15.
+//  Copyright (c) 2015 Rémy Virin. All rights reserved.
+//
+
+// We just add this Objective-C++ file to force the link with C++ standard librairies. 
